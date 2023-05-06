f6c36f2209794533955e64f5b3f567a0c5b65857
Maycon Assis <may.con.fa@hotmail.com>
2023-05-06 17:00:38 +1200

    started working on form

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,12 @@
+import Form from './Form'
 import Home from './Home'
 import Navbar from './Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className="flex flex-col h-screen bg-gray-300 items-center gap-96">
       <Navbar />
+      <Form />
     </div>
   )
 }
--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,9 +1,9 @@
 function Navbar() {
   return (
-    <div className="flex justify-around items-center">
-      <h2>NEW</h2>
+    <div className="flex justify-around items-center text-gray-600 w-screen">
+      <h2 className="text-4xl">NEW</h2>
       <h1 className="text-8xl">VERTICAL VAULT</h1>
-      <h2>OPEN</h2>
+      <h2 className="text-4xl">OPEN</h2>
     </div>
   )
 }
