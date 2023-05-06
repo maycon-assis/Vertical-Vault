6fd788459d65e01f8a8153eebcd9113355833528
Maycon Assis <may.con.fa@hotmail.com>
2023-05-06 16:08:06 +1200

    all config done and main setup. Initial commit

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,26 +1,11 @@
-import { useEffect } from 'react'
-import { useAppDispatch, useAppSelector } from '../hooks'
-import { fetchFruits } from '../slices/fruits'
+import Home from './Home'
+import Navbar from './Navbar'
 
-function App() {
-  const fruits = useAppSelector((state) => state.fruits)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    dispatch(fetchFruits())
-  }, [dispatch])
-
+const App = () => {
   return (
-    <>
-      <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>
-          {fruits.map((fruit) => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./server/public/index.html', './client/**/*.{html,js,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
