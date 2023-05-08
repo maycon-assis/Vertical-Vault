d1459779578560a0b40220e55b7376067e7c1502
Maycon Assis <may.con.fa@hotmail.com>
2023-05-08 15:28:24 +1200

    remove navbar component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,9 @@
-import Form from './Form'
 import Home from './Home'
-import Navbar from './Navbar'
 
 const App = () => {
   return (
-    <div className="flex flex-col h-screen bg-gray-300 items-center gap-96">
-      <Navbar />
-      <Form />
+    <div className="flex flex-col h-screen bg-gray-300 items-center">
+      <Home />
     </div>
   )
 }
